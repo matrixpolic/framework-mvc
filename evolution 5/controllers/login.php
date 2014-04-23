b835d4f6a7b782f1d3829c1e4caadddfec37627d
Nikoquattro <nikoquattro@gmail.com>
2014-04-23 21:35:07 +0200

    Début évolution 5

---
+++ evolution 5/controllers/login.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* Classe Login qui étend la classe Controller
+*
+* Classe Login qui étend la classe Controller
+*
+* LICENSE: Informations sur la licence
+*
+* @copyright  Copyright (c) 2014 Quattro
+* @license    BSD License
+* @version    1.0
+* @link       
+* @since      1.0
+*/
+
+class Login extends Controller {
+
+	function __construct() {
+		parent::__construct();
+	}
+
+	function index() {
+		$this->view->render('login/index');
+	}
+
+	function run() {
+		$this->model->run();
+	}
+
+}
---
+++ evolution 5/index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* Fichier qui sert d'unique porte d'entrée du framework
+*
+* Fichier qui sert d'unique porte d'entrée du framework. On charge les quelques classes principales et on instancie notre bottstrap.
+*
+* LICENSE: Informations sur la licence
+*
+* @copyright  Copyright (c) 2014 Quattro
+* @license    BSD License
+* @version    1.0
+* @link       
+* @since      1.0
+*/
+
+// Use an autoloader!
+require 'libs/Bootstrap.php';
+require 'libs/Controller.php';
+require 'libs/Model.php';
+require 'libs/View.php';
+
+// Library
+require 'libs/Database.php';
+require 'libs/Session.php';
+
+require 'config/paths.php';
+require 'config/database.php';
+
+$app = new Bootstrap();
